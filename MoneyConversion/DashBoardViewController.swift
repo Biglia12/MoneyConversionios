e51720860313deef9085dbae3423f00ddef43f7a
Biglia12 <ezequielbiglia12@gmail.com>
2023-05-15 12:12:39 -0300

    color primario

--- MoneyConversion/DashBoardViewController.swift
+++ MoneyConversion/DashBoardViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 class DashBoardViewController: UIViewController {
 
     @IBOutlet weak var txtLabel: UILabel!
+    
+    //let tabBar: UITabBar = UITabBar()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Cambiar el color de la UITabBar
+           //let tabBarColor = UIColor.red
+        //self.tabBarController?.tabBar.barTintColor = UIColor.red
+           
+           // Establecer el color de los íconos no seleccionados
+           //let unselectedColor = UIColor.red.withAlphaComponent(0.6)
+          // self.tabBarController?.tabBar.unselectedItemTintColor = UIColor.red
+        
+        print("viewDidLoad executed successfully")
         
         txtLabel.text = "TextDash"
         
